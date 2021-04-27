d78520db5984d514b76c75570370db7562d876cb
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-04-27 13:47:53 +0530

    added c

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Compudex
+# CompuDex
+A pokédex of programming languages.
+
 ## Script Format
 ```
 # Single line comment - This is an example of ___ language
 
 function main
-    Take two numbers input in for loop
-    Take operator char as input
+    Take two numbers as input in for loop
+    Take operator as char input
     Depending on operator perform operation in if else ladder
-    Write output to file (filename: output.txt)
+    Write result to file (filename: output.txt)
 endfunction
 
 call main()
---
+++ src/C.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+// This is an example of the C language
+int main(){
+    int arr[2];
+
+    for(int i=0;i<2;i++){
+        printf("Enter number %d\n", i+1);
+        int num;
+        scanf("%d", &num);
+        arr[i] = num;
+    }
+
+    char operator;
+    printf("Enter operator\n");
+    scanf("%*c%c", &operator); // %*c discards preceeding newline
+
+    int result;
+    if(strncmp(&operator, "+", 1) == 0){
+        result = arr[0] + arr[1];
+    }
+    else if(strncmp(&operator, "*", 1) == 0){
+        result = arr[0] * arr[1];
+    }
+    else if(strncmp(&operator,"-",1) == 0){
+        if(arr[0] >= arr[1]){
+            result = arr[0] - arr[1];
+        }
+        else{
+            result = arr[1] - arr[0];
+        }
+    }
+    else if(strncmp(&operator, "/", 1) == 0){
+        result = arr[0] / arr[1];
+    }
+    else{
+        printf("This operator has not been included.");
+        result = INFINITY;
+    }
+
+    FILE *handle = fopen("output.txt", "w+");
+    if(result != INFINITY){
+        fprintf(handle, "%d\n", result);
+    }
+    else{
+        fprintf(handle, "None");
+    }
+    fclose(handle);
+
+    return 0;
+}
+
+/* Usage:
+ * 1. Compile the script with gcc C.c
+ * 2. You should now have an a.out file in the same directory
+ * 3. Execute compiled binary with ./a.out
+ * 4. Result will be written to output.txt
+ */
