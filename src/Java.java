bfb24f7cdab0dff83669dd784183e0ca72bd9f2a
PratikGarai <pratikgarai0208@gmail.com>
2021-04-27 15:43:47 +0530

    added java

---
+++ src/Java.java
@@ -0,0 +1,48 @@
+// The is an example of Java language
+
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Java {
+	public static void main(String[] args) throws IOException
+	{
+		Scanner in = new Scanner(System.in);
+
+		int[] a = new int[2];
+		for(int i=0; i<2; i++)
+			a[i] = in.nextInt();
+
+		char op = in.next().charAt(0);
+		boolean flag = true;
+		int result = 0;
+		if(op=='+')
+			result = a[0]+a[1];
+		else if(op=='-')
+			result = (a[0]>=a[1])?(a[0]-a[1]):(a[1]-a[0]);
+		else if(op=='*')
+			result = a[0]*a[1];
+		else if(op=='/')
+			result = a[0]/a[1];
+		else {
+			System.out.println("This operator is not included yet");
+			flag = false;
+		}
+
+		if(flag) {
+			FileWriter f = new FileWriter("output.txt");
+			f.write(Integer.toString(result));
+			f.close();
+		}
+	}
+}
+
+/* 
+ * Usage : 
+ * 1. Install JDK on your system and ensure that the bin
+ * 	directory that contains the compiler is in your 
+ * 	env path.
+ * 2. Run the command :
+ * 	java Java.java
+ *
+ * */
